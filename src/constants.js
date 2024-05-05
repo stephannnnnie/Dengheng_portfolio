a91b753973c0e7e8c392e647b60ec6d8e6a143b5
stephannnnnie <71458749+stephannnnnie@users.noreply.github.com>
2024-05-05 15:12:32 +0800

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,34 +1,18 @@
 export const scaleFactor = 2;
 
 export const dialogueData = {
-    PC: `My PC, my everything. I always use Python or Java to implement my thoughts. If you are interested \
-    in my past projects, here is my <a href="https://github.com/stephannnnnie" target="_blank">Github</a>!\
-     Together, we tackle coding conundrums, embark on gaming odysseys, and binge-watch every episode of \
-     'The Big Bang Theory' on repeat.`,
-    CSdegree: `I got my bachelor's degree in computer science at SUSTech and now I am still studying CS at USC.\
-    I enjoyed both journeies though there were some struggles.`,
-    backpack:`It's my backpack to adventure, my escape from the hustle and bustle of everyday life. On the \
-    trails, every step brings a new surprise, from unexpected encounters to breathtaking views. Even with a \
-    mountain of work, I make time to hike monthly. It's the unpredictability and the possibility of encountering \
-    something magical that keeps drawing me back to the trails."`,
-    exit:`If you want to exit Dengheng's portfolio, just close the tab`,
-    bookshelf:`There are various books on my shelf, from programming, social science to fiction. Recently I am reading \
-    World Power Made Easy, where I accumulate vocabulary. Like old friends, books never lose their charm \
-    and surprise me every time I pick them up.`,
-    stereo:`My Stereo System: With every note that fills the air, it's like flipping a switch that ignites my \
-    soul and sets my mind adrift. Whether I'm coding or simply lost in thought, the soundtrack \
-    of music keeps my creativity flowing and my focus razor-sharp.`,
+    PC: `My PC, my everything.`,
+    CSdegree: `I enjoyed both journeies though there were some struggles.`,
+    backpack:`It's my backpack to adventure, my escape from the hustle and bustle of everyday life."`,
+    exit:`If you want to exit my portfolio, just close the tab`,
+    bookshelf:`There are various books on my shelf.`,
+    stereo:`My Stereo System.`,
     bed:`Have a rest. A new day has come!`,
-    table:`This is my desk and on it is my resume. You can ckeck it through \
-    <a href="https://www.linkedin.com/in/denghengshi/" target="_blank">LinkedIn</a>. Contact me at dengheng@usc.edu \
-    if you have any interesting job opportunities!`,
-    fridge:`As a passionate cook and food enthusiast, my refrigerator is always stocked with fresh ingredients, \
-    ready to be transformed into delicious meals. Whether I'm whisking up a comforting soup or crafting a gourmet \
-    feast, the act of cooking is not just a hobby, but a therapeutic escape that nourishes both body and soul.`,
+    table:`This is my desk and on it is my resume.`,
+    fridge:`As a passionate cook and food enthusiast...`,
     bench:`Sit down with me, my friend! Let's have a relaxing conversation about how you feel and share \
     anything with me.`,
-    unknown:`Oh no. You found this. Then you have access to my <a href="https://space.bilibili.com/436406151" target="_blank">Bilibili space</a>`,
-    TV:`For me, TV is a projector for movies and an entertainment hub, capable of transforming any room into a karaoke stage.`,
+    unknown:`Oh no. You found this.`,
     scheduler:`Having a scheduler in hand makes me feel comforting and clear-headed.`,
     photos:`Updating...`
 }
